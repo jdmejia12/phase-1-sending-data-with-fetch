3cbabd55c3ee7c688aca77e9035909c7d6ffe793
JD <jdmejia712@gmail.com>
2021-11-17 11:16:13 -0500

    commit tree 216900b8c42e9287ac95ec261a195b2d7fcb8a24

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+let bodyBox = document.querySelector('body')
+
+function submitData(name, email){
+    const user = {name : name, email: email}
+    //console.log(JSON.stringify(user))
+    return fetch(`http://localhost:3000/users`, {
+        
+        method:'POST',
+        headers: {'Content-Type':'application/json', 
+            Accept:'application/json'}, 
+        body:JSON.stringify(user)
+    })
+    .then(re=>{return re.json()})
+    .then(posted=>bodyBox.append(posted.id))
+    .catch(error => bodyBox.append(error.message))
+}
+submitData()
